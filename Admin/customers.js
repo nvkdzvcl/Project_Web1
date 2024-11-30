126708e75b3505846274ab9accb75deead0cbff8
ving <ndvinh360@gmail.com>
2024-11-30 16:20:34 +0700

    Thêm role trong customers.js

--- Admin/customers.js
+++ Admin/customers.js
@@ -8,61 +8,71 @@ if(!customers) {
             id: 1,
             username: '111',
             password: '111',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 2,
             username: '222',
             password: '222',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 3,
             username: 'caovn',
             password: '333',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 4,
             username: 'hainguyen',
             password: 'hainguyen',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 5,
             username: 'ngohongvi',
             password: 'vingo',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 6,
             username: 'jack',
             password: '9797',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 7,
             username: 'diepvien007',
             password: 'diepvien007',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 8,
             username: 'thongtindaotao',
             password: 'mssv',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 9,
             username: 'nguyenvana',
             password: 'nguyenvana',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
         {
             id: 10,
             username: '888',
             password: 'maso8',
-            status: 'true'
+            status: 'true',
+            role: 'customer'
         },
     ]
     // Lưu dữ liệu mặc định vào localStorage
